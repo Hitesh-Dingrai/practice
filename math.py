b05918f39e07c57d5c9f01f73a5310dc8ceab1bd
Hitesh-Dingrai <91075197+Hitesh-Dingrai@users.noreply.github.com>
2022-01-27 16:38:03 +0530

    future commit

--- math.py
+++ math.py
@@ -2,7 +2,7 @@ def add(x,y):                      #local repository working
   return x+y
 
 def sub(x,y):
-  return x-y
+  return x-y-z                   #this is future branch
 
 def multiply(x,y):
   return x-y                                      #commit on remote repositoru
